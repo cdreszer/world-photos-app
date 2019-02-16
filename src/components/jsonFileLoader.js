be0af44cd35cdf243eaebe559039ca3626199a35
Chase Dreszer <chasedreszer@Chases-MacBook-Pro.local>
2019-02-16 12:01:39 -0800

    Modified bio introduction pic name and removed unused files.

--- src/components/jsonFileLoader.js
+++
@@ -1,8 +0,0 @@
-//image loader
-
-// wrtie a function to extract ^^^ json from file
-export function jsonFileLoader() {
-    // let data = require('./images.JSON');
-    // console.log(data.directories);
-    // return data.directories;
-}
