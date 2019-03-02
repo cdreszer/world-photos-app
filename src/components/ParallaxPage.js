f6afe5aa9174a97dbd8a538718e8f2c462b33dc8
Chase Dreszer <chasedreszer@Chases-MacBook-Pro.local>
2019-03-02 15:15:08 -0800

    Allow passing in of images for parallax page.
    Previously was just hard coding the three images inside css file.
    
    Accepts props for three images (top/middle/bottom) and two descriptions.

--- src/components/ParallaxPage.js
+++ src/components/ParallaxPage.js
@@ -5,21 +5,51 @@ import './../css/ParallaxPage.css';
 
 
 /**
+    Returns an in-line style object setting the background image 
+    to the image for the passed image path if defined.
+*/
+function getBackgroundStyle(imagePath) {
+  var topImageURI = "";
+
+  if (imagePath) {
+    topImageURI = require(`./../images/${imagePath}`)
+  }
+
+  return topImageURI.length ? { backgroundImage: `url(${topImageURI})`} : {};
+}
+
+/**
    Parallax page to show off some waves.
+
+   Accepts props for three images (top/middle/bottom) and two descriptions...
+   descriptions need to be in JSX format.
 */
 function ParallaxPage(props) {
+  //in-line style to add background image... could be used if want to randomize the photos.
+  const topImageStyle = getBackgroundStyle(props.topImage);
+  const middleImageStyle = getBackgroundStyle(props.middleImage);
+  const bottomImageStyle = getBackgroundStyle(props.bottomImage);
+  const desc1 = props.desc1 ? props.desc1 : 
+    (<p> Make no mistake: Supertubes is one of the best waves in Europe.<br/> 
+      There’s one main peak with a super-fast (though makable) left, and a somewhat shorter right, 
+      and one other peak down the beach that’s not quite as perfect but still way better than your 
+      average day at Huntington Pier. </p>);
+  const desc2 = props.desc2 ? props.desc2 : 
+    (<p> The best known surf spot in north Portugal breaks off a jetty and runs 
+      through to the inside as a fast, walled up right that finally dies as a close out over 
+      shallow rocks. Plenty of tubing moments with swells of up to 3m+ (10ft).</p> );
+
   return (
      <React.Fragment>
-        <div className="peniche" />
+        <div style={topImageStyle} className="top-image" />
         <div className="parallax-caption">
-           <p> Make no mistake: Supertubes is one of the best waves in Europe.<br/> There’s one main peak with a super-fast (though makable) left, and a somewhat shorter right, and one other peak down the beach that’s not quite as perfect but still way better than your average day at Huntington Pier. 
-           </p> 
+           {desc1}
         </div>
-        <div className="espinho"/>
+        <div style={middleImageStyle} className="middle-image"/>
         <div className="parallax-caption">
-           <p> The best known surf spot in north Portugal breaks off a jetty and runs through to the inside as a fast, walled up right that finally dies as a close out over shallow rocks. Plenty of tubing moments with swells of up to 3m+ (10ft).</p> 
+           {desc2}
         </div>
-        <div className="changing"/>
+        <div style={bottomImageStyle} className="bottom-image"/>
      </React.Fragment>
   );
 }
